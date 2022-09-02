0d31818dc3b98668105061567cd36fef65acd9bd
Lekan <jimoridwanlekan@gmail.com>
2022-09-02 10:13:18 +0100

    Add Categories index and show pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,7 +18,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: 'Category was successfully created.' }
+        format.html { redirect_to categories_path, notice: 'Category was successfully created.' }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -45,7 +45,7 @@ class CategoriesController < ApplicationController
     @category.destroy
 
     respond_to do |format|
-      format.html { redirect_to categories_url, notice: 'Category was successfully destroyed.' }
+      format.html { redirect_to categories_url, notice: 'Category was successfully deleted.' }
       format.json { head :no_content }
     end
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,7 @@
 class Category < ApplicationRecord
+  validates :name, presence: true
+  validates :icon, presence: true
+
   belongs_to :user
   has_many :category_records, dependent: :destroy
   has_many :records, through: :category_records
